9917df3fbcd2278659da7393b216adc93a376969
WittHub <rasmus.wittrock@gmail.com>
2024-04-24 15:05:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pywizlight import wizlight, PilotBuilder, discovery
 
 load_dotenv()
 
-API_ENDPOINT = "http://" + str(os.getenv('SERVER_HOST_URL')) + "/meters"
+API_ENDPOINT = "http://" + str(os.getenv('SERVER_HOST_URL'))
 
 measurement_delay = int(os.getenv('MEASUREMENT_DELAY'))
 discover_delay = int(os.getenv('DISCOVER_DELAY'))
@@ -20,7 +20,7 @@ async def main():
    while True:
       for i in range(1, discover_delay//measurement_delay):
          await asyncio.sleep(measurement_delay)
-         await measure(bulbs, API_ENDPOINT)  
+         await measure(bulbs, API_ENDPOINT+"/measurements")  
       await discover_bulbs()
    
 async def discover_bulbs():
@@ -28,6 +28,18 @@ async def discover_bulbs():
    print("Discovering bulbs")
    global bulbs
    bulbs = await discovery.discover_lights(broadcast_space="10.10.255.255")
+   for bulb in bulbs:
+      r = requests.post(url = API_ENDPOINT+"/smartplugs", json = {'id': str(bulb.mac), 'name': str(bulb.mac)})
+      #check error code
+      if r.status_code == 200:
+         print("Bulb already in database")
+      elif r.status_code == 201:
+         print("Bulb added to database")
+      else:
+         print("Error adding bulb to database")
+         print(r.status_code)
+         print(r.text)
+         pass
 
 async def measure(bulbs, api_endpoint):
    print()
@@ -35,8 +47,9 @@ async def measure(bulbs, api_endpoint):
    for bulb in bulbs:
       try :
          await send_data(bulb, api_endpoint)
-      except :
-         print("Error sending data")
+      except Exception as e:
+         print("Error measuring bulb")
+         print(e)
          pass
 
 async def send_data(bulb, api_endpoint):
@@ -45,11 +58,11 @@ async def send_data(bulb, api_endpoint):
    timestamp = datetime.now().isoformat()
    bulb_power = await bulb.get_power()
    data = {
-      'bulpMac': str(bulb.mac),
+      'smartPlugId': str(bulb.mac),
       'wattage': bulb_power,
       'timestamp': timestamp
    }
-   r = await requests.post(url = api_endpoint, json = data)
+   r = requests.post(url = api_endpoint, json = data)
       
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
